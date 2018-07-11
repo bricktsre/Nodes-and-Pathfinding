29c73aadf72c8465afd0ebb0dc49a37fc46f6d91
gwood <gwood@10.0.0.225>
2018-07-10 22:52:50 -0400

    New character class
    Character will eventually move between the nodes but for right now
    stayswhere it is

---
+++ Character.java
@@ -0,0 +1,16 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Character {
+	private int row,col;
+	
+	public Character(int x, int y) {
+		row = y;
+		col = x;
+	}
+	
+	public void draw(Graphics g) {
+		g.setColor(Color.YELLOW);
+		g.fillOval(row*50-13, col*50-13, 26, 26);
+	};
+}
