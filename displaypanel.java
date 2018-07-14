d48b60e5e99bacc94e8567c6a5679fb3d6f05f17
Greg <Greg@WOODWARD-GREG>
2018-07-14 01:13:38 -0400

    Reads in graph from a file
    New constructor string parameter that is the name of the file
    first the number of nodes is read in and used to create the array of
    nodes
    second the nodes are created and added to the nodes array
    finally each node is given its neighbors

--- displaypanel.java
+++ displaypanel.java
@@ -4,49 +4,44 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 import javax.swing.JPanel;
 
 
 @SuppressWarnings("serial")
 public class displaypanel extends JPanel{
-	private Node[] nodes = new Node[5];
+	private Node[] nodes;
 	private Character c;
 	private javax.swing.Timer timer;
 	
-	public displaypanel() {
+	public displaypanel(String s) {
 		setBackground(Color.BLACK);
 		setLayout(null);
-		initializeNodes();
+		initializeNodes(s);
 		c = new Character(5,5, nodes[1]);
 		
 		timer = new javax.swing.Timer(40, new TimerListener());
 		timer.start();
 	}
 	
-	private void initializeNodes() {
-		Node node1 = new Node(1,1);
-		Node node2 = new Node(5,5);
-		Node node3 = new Node(1,5);
-		Node node4 = new Node(5,1);
-		Node node5 = new Node(8,5);
-		
-		node1.addNeighbors(node4, 4);
-		node1.addNeighbors(node3, 4);
-		node2.addNeighbors(node4, 4);
-		node2.addNeighbors(node5, 3);
-		node2.addNeighbors(node3, 4);
-		node3.addNeighbors(node1, 4);
-		node3.addNeighbors(node2, 4);
-		node4.addNeighbors(node2, 4);
-		node4.addNeighbors(node1, 4);
-		node5.addNeighbors(node2, 3);
-		
-		nodes[0]= node1;
-		nodes[1]=node2;
-		nodes[2]=node3;
-		nodes[3]=node4;
-		nodes[4]=node5;
+	private void initializeNodes(String s) {
+		try {
+			Scanner a = new Scanner(new File(s));
+			nodes = new Node[a.nextInt()];
+			for(int i =0;i<nodes.length;i++)
+				nodes[i]= new Node(a.nextInt(),a.nextInt());
+			for(int i =0;i<nodes.length;i++) {
+				while(!a.hasNext("-1"))
+					nodes[i].addNeighbors(nodes[a.nextInt()-1], a.nextInt());
+				a.nextInt();
+			}
+			a.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	private class TimerListener implements ActionListener{				//reference to gamePanel
