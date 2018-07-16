c9ca05818d876a1d748dad6f3a9c433b102e8707
Greg <Greg@WOODWARD-GREG>
2018-07-16 19:15:59 -0400

    Added class variables, changed the move method, added a new method
    Now has x,y coordinates, a current node, a target node, and a set speed
    Move checks if the character is at the target node, if not the x or y is
    updated to move it closer to the target node

--- Character.java
+++ Character.java
@@ -2,23 +2,40 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Character {
-	private int row,col;
+	final int speed = 1;
+	private int x,y;
 	private Node nodeAt;
+	private Node targetNode;
 	
 	public Character(int x, int y, Node n) {
-		row = y;
-		col = x;
-		n = nodeAt;
+		this.x = x;
+		this.y = y;
+		nodeAt=n;
 	}
 	
-	public void move(Node n) {
-		nodeAt = n;
-		row= nodeAt.getRow();
-		col= nodeAt.getCol();
+	public void move() {
+		if(nodeAt.compareTo(targetNode)==0)
+			return;
+		else if((x/25==targetNode.getCol()) && (y/25==targetNode.getRow()) &&(x%25==0) && (y%25==0))
+			nodeAt= targetNode;
+		else {
+			if(nodeAt.getRow()<targetNode.getRow())
+				y+=speed;
+			else if(nodeAt.getRow()>targetNode.getRow())
+				y-=speed;
+			else if(nodeAt.getCol()<targetNode.getCol())
+				x+=speed;
+			else if(nodeAt.getCol()>targetNode.getCol())
+				x-=speed;
+		}
+	}
+	
+	public void setTarget(Node n) {
+		targetNode = n;
 	}
 	
 	public void draw(Graphics g) {
 		g.setColor(Color.GREEN);
-		g.fillOval(row*25-6, col*25-6, 13, 13);
+		g.fillOval(x-6, y-6, 13, 13);
 	};
 }
