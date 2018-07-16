fa8709990faaad460d073f38f7a7ccc83275c0e2
Greg <Greg@WOODWARD-GREG>
2018-07-16 01:20:11 -0400

    Implemented the comparable interface
    If the row and col are equal than the nodes are equal and returns 0
    If the row is greater or equal and the col is lessor or vice versa the
    node is less than returning -1
    Otherwise the node is greater and returns 1

--- Node.java
+++ Node.java
@@ -3,8 +3,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayList;
 
-public class Node {
-	int row,col;
+public class Node implements Comparable<Node>{
+	private int row,col;
 	ArrayList<Edge> neighbors = new ArrayList<Edge>();
 	
 	public Node(int x, int y) {
@@ -30,5 +30,14 @@ public class Node {
 			g.drawLine(col*25, row*25, e.getNode().getCol()*25, e.getNode().getRow()*25);
 		g.setColor(Color.WHITE);
 		g.fillOval(col*25-7,row*25-7,15,15);
+	}
+
+	@Override
+	public int compareTo(Node o) {
+		if(row==o.getRow()&&col==o.getCol())
+			return 0;
+		else if((row>=o.getRow()&&col<o.getCol())||(row<o.getRow()&&col>=o.getCol()))
+			return -1;
+		return 1;
 	}
 }
